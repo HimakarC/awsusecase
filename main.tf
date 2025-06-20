7ff7d7578e0bcd505292ab7a8fd148dbf15dd71d
HimakarC <116370824+HimakarC@users.noreply.github.com>
2025-06-20 21:54:51 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -42,6 +42,14 @@ resource "aws_s3_bucket_policy" "static_website_bucket_policy" {
   })
 }
 
+resource "aws_s3_bucket_public_access_block" "public_access" {
+  bucket = aws_s3_bucket.my_terraform_bucket.id
+  block_public_acls  = false
+  block_public_policy = false
+  ignore_public_acls = false
+  restrict_public_buckets = false
+}
+
 resource "aws_s3_object" "index_html" {
   bucket = aws_s3_bucket.my_terraform_bucket.id
   key = "index.html"
