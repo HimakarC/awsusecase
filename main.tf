2f997b1902e44fac2fa93135f0821effce0b33a1
HimakarC <116370824+HimakarC@users.noreply.github.com>
2025-06-20 12:00:54 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,8 +3,53 @@ provider "aws" {
 }
 
 resource "aws_s3_bucket" "static_site" {
-  bucket = "my-static-site-bucket-123"
-  acl = "public-read"
+  bucket = "my-static-website-20250620"  # Must be globally unique
+  acl    = "public-read"
+
+  website {
+    index_document = "index.html"
+    error_document = "error.html"
+  }
+
+  tags = {
+    Name        = "StaticWebsite"
+    Environment = "Dev"
+  }
 }
 
-# Add policies, CodeBuild project, and CodePipeline here...
+resource "aws_s3_bucket_policy" "static_site_policy" {
+  bucket = aws_s3_bucket.static_site.id
+
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Sid       = "PublicReadGetObject",
+        Effect    = "Allow",
+        Principal = "*",
+        Action    = "s3:GetObject",
+        Resource  = "${aws_s3_bucket.static_site.arn}/*"
+      }
+    ]
+  })
+}
+
+resource "aws_s3_bucket_object" "index" {
+  bucket = aws_s3_bucket.static_site.id
+  key    = "index.html"
+  source = "index.html"
+  acl    = "public-read"
+  content_type = "text/html"
+}
+
+resource "aws_s3_bucket_object" "error" {
+  bucket = aws_s3_bucket.static_site.id
+  key    = "error.html"
+  source = "error.html"
+  acl    = "public-read"
+  content_type = "text/html"
+}
+
+output "website_url" {
+  value = aws_s3_bucket.static_site.website_endpoint
+}
