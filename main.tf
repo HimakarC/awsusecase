23723df1e8349f11ab4535cca2b639fb6e213e5e
HimakarC <116370824+HimakarC@users.noreply.github.com>
2025-06-20 10:42:00 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,25 +3,8 @@ provider "aws" {
 }
 
 resource "aws_s3_bucket" "static_site" {
-  bucket = "my-static-site-bucket-123"
-  website {
-    index_document = "index.html"
-  }
+  bucket = "nothinghimakar"
   acl = "public-read"
-}
-
-resource "aws_iam_role" "codebuild_role" {
-  name = "codebuild-service-role"
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17",
-    Statement = [{
-      Effect = "Allow",
-      Principal = {
-        Service = "codebuild.amazonaws.com"
-      },
-      Action = "sts:AssumeRole"
-    }]
-  })
 }
 
 # Add policies, CodeBuild project, and CodePipeline here...
