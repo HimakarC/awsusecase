d8ece6737b81838f825acfb16a686acd465a8f69
HimakarC <116370824+HimakarC@users.noreply.github.com>
2025-06-20 10:56:43 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,10 +1,13 @@
 provider "aws" {
-  region = "us-west-2"
+  region = "us-east-1"
 }
 
-resource "aws_s3_bucket" "static_site" {
-  bucket = "nothinghimakar"
-  acl = "public-read"
-}
+resource "aws_s3_bucket" "example" {
+  bucket = "my-unique-bucket-name-20250620"
+  acl    = "private"
 
-# Add policies, CodeBuild project, and CodePipeline here...
+  tags = {
+    Name        = "MyBucket"
+    Environment = "Dev"
+  }
+}
