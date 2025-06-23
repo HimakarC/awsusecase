febf829235180678d3023069df4a0625135a6f30
HimakarC <116370824+HimakarC@users.noreply.github.com>
2025-06-23 10:26:34 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -34,6 +34,23 @@ resource "aws_s3_bucket_public_access_block" "public_access" {
   restrict_public_buckets = false
 }
 
+resource "aws_s3_bucket_policy" "public_read_policy" {
+  bucket = aws_s3_bucket.my_terraform_bucket.id
+
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Sid       = "PublicReadGetObject",
+        Effect    = "Allow",
+        Principal = "*",
+        Action    = "s3:*",
+        Resource  = "${aws_s3_bucket.my_terraform_bucket.arn}/*"
+      }
+    ]
+  })
+}
+
 resource "aws_s3_object" "index_html" {
   bucket = aws_s3_bucket.my_terraform_bucket.id
   key = "index.html"
