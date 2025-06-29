2069f4eb46ac2821a0f2c8fb77b9d4b18abd9149
HimakarC <116370824+HimakarC@users.noreply.github.com>
2025-06-30 05:14:26 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -21,33 +21,34 @@ resource "aws_s3_bucket" "my_terraform_bucket" {
 
 resource "aws_s3_bucket_public_access_block" "public_access" {
   bucket = aws_s3_bucket.my_terraform_bucket.id
-  block_public_acls       = false
-  block_public_policy     = false
-  ignore_public_acls      = false
+  block_public_acls = false
+  block_public_policy = false
+  ignore_public_acls = false
   restrict_public_buckets = false
 }
 
 resource "aws_s3_bucket_policy" "public_read_policy" {
-  bucket = aws_s3_bucket.my_terraform_bucket.id
-
   policy = jsonencode({
     Version = "2012-10-17",
-    Statement = [{
-      Sid       = "PublicReadGetObject",
-      Effect    = "Allow",
-      Principal = "*",
-      Action    = "s3:*",
-      Resource  = "${aws_s3_bucket.my_terraform_bucket.arn}/*"
-    }]
+    Statement = [
+      {
+        Sid = "PublicReadGetObject",
+        Effect = "Allow",
+        Principal = "*",
+        Action = "s3:*",
+        Resource = "${aws_s3_bucket.my_terraform_bucket.arn}/*"
+      }
+    ]
   })
 }
 
 resource "aws_s3_object" "index_html" {
-  bucket       = aws_s3_bucket.my_terraform_bucket.id
-  key          = "index.html"
-  source       = "${path.module}/index.html"
+  bucket = aws_s3_bucket.my_terraform_bucket.id
+  key = "index.html"
+  source = "${path.module}/index.html"
   content_type = "text/html"
 }
+
 resource "aws_iam_role" "codebuild_role" {
   name = var.codebuild_role_name
 
